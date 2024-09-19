108e41737e523fbd42f0c225b24ec5b24dd8bd11
Pramila Shanmugam <pramila.sl@gmail.com>
2024-09-19 19:19:49 +0000

    installation

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -44,12 +44,20 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages', 
+    'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
-    'django_filters'
+    'django_filters',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
+
     'profiles',
     'posts',
     'comments',
@@ -57,7 +65,7 @@ INSTALLED_APPS = [
     'followers',
     
 ]
-
+SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
