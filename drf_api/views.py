ddf2134f8867450cb2000cc8c23a87b7e90cb658
Pramila Shanmugam <pramila.sl@gmail.com>
2024-09-25 13:18:47 +0000

    deployment to heroku

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 
 @api_view()
@@ -8,3 +9,9 @@ def root_route(request):
         "message": "Welcome to my drf API!"
     })
 
+@api_view(['POST'])
+def logout_route(request):
+    # You can customize this logic for logging out, e.g., deleting a token or session.
+    return Response({
+        "message": "Logged out successfully"
+    }, status=status.HTTP_200_OK)
